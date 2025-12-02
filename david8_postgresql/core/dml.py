24f2b67262ee7525e405233f4f87e1a35c156399
Danila Ganchar <danila.ganchar@gmail.com>
2025-12-02 18:07:47 +0100

    #3 SELECT FOR ...

--- david8_postgresql/core/dml.py
+++ david8_postgresql/core/dml.py
@@ -1,9 +1,82 @@
 import dataclasses
 
-from david8.core.base_dml import BaseUpdate, TargetTableConstruction
+from david8.core.base_dml import BaseSelect, BaseUpdate, TargetTableConstruction, log_and_reset
 from david8.protocols.dialect import DialectProtocol
 
-from david8_postgresql.protocols.dml import UpdateProtocol
+from ..protocols.dml import SelectProtocol, UpdateProtocol
+
+
+@dataclasses.dataclass(slots=True)
+class Select(BaseSelect, SelectProtocol):
+    row_lock_mode: str = ''
+
+    def for_key_share(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR KEY SHARE'
+        return self
+
+    def for_key_share_nw(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR KEY SHARE NOWAIT'
+        return self
+
+    def for_key_share_sl(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR KEY SHARE SKIP LOCKED'
+        return self
+
+    def for_share(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR SHARE'
+        return self
+
+    def for_share_nw(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR SHARE NOWAIT'
+        return self
+
+    def for_share_sl(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR SHARE SKIP LOCKED'
+        return self
+
+    def for_update(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR UPDATE'
+        return self
+
+    def for_update_nw(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR UPDATE NOWAIT'
+        return self
+
+    def for_update_sl(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR UPDATE SKIP LOCKED'
+        return self
+
+    def for_nk_update(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR NO KEY UPDATE'
+        return self
+
+    def for_nk_update_nw(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR NO KEY UPDATE NOWAIT'
+        return self
+
+    def for_nk_update_sl(self) -> 'SelectProtocol':
+        self.row_lock_mode = 'FOR NO KEY UPDATE SKIP LOCKED'
+        return self
+
+    @log_and_reset
+    def get_sql(self, dialect: DialectProtocol = None) -> str:
+        """
+        TODO: move to _get_sql() on david8 side
+        """
+        dialect = dialect or self.dialect
+        with_query = self._with_queries_to_sql(dialect)
+        select = self._columns_to_sql(dialect)
+        from_ref = self._from_to_sql(dialect)
+        joins = self._joins_to_sql(dialect)
+        where = self.where_construction.get_sql(dialect)
+        group_by = self._group_by_to_sql(dialect)
+        having = self._having_to_sql(dialect)
+        union = self._union_to_sql(dialect)
+        order_by = self._order_by_to_sql()
+
+        limit = f' LIMIT {self.limit_value}' if self.limit_value else ''
+        sql =  f'{with_query}SELECT {select}{from_ref}{joins}{where}{group_by}{order_by}{having}{limit}{union}'
+        return f'{sql} {self.row_lock_mode}' if self.row_lock_mode else sql
 
 
 @dataclasses.dataclass(slots=True)
@@ -42,4 +115,3 @@ class Update(BaseUpdate, UpdateProtocol):
         self.from_table_constr.set_source(table_name, db_name)
         self.from_table_alias = alias
         return self
-
--- david8_postgresql/protocols/query_builder.py
+++ david8_postgresql/protocols/query_builder.py
@@ -1,8 +1,12 @@
 from david8.protocols.query_builder import QueryBuilderProtocol as _QueryBuilderProtocol
+from david8.protocols.sql import AliasedProtocol, ExprProtocol, FunctionProtocol
 
-from .dml import UpdateProtocol
+from .dml import SelectProtocol, UpdateProtocol
 
 
 class QueryBuilderProtocol(_QueryBuilderProtocol):
     def update(self) -> UpdateProtocol:
+        pass
+
+    def select(self, *args: str | AliasedProtocol | ExprProtocol | FunctionProtocol) -> SelectProtocol:
         pass
