b99f9ea4d2700e4ef4f4690df998916b5f2279af
Danila Ganchar <danila.ganchar@gmail.com>
2025-12-18 11:44:39 +0100

    #7 array_remove

--- david8_postgresql/functions_array.py
+++ david8_postgresql/functions_array.py
@@ -1,3 +1,5 @@
-from david8_postgresql.core.functions_array_factory import StrSeparatedArgsFnFactory as _StrSeparatedArgsFnFactory
+from david8_postgresql.core.functions_array_factory import Col2AnyArgsFactory as _Col2AnyArgsFactory
+from david8_postgresql.core.functions_array_factory import StrSeparatedArgsFactory as _StrSeparatedArgsFactory
 
-unnest = _StrSeparatedArgsFnFactory(name='unnest')
+unnest = _StrSeparatedArgsFactory(name='unnest')
+array_remove = _Col2AnyArgsFactory(name='array_remove')
